c0435704db755a2a20f7cf369389762ee5191c74
pepeette <59664130+pepeette@users.noreply.github.com>
2023-02-21 09:23:36 +0700

    bot pgme

---
+++ main.py
@@ -0,0 +1,44 @@
+from keep_alive import keep_alive
+import discord
+import os
+import requests
+import json
+import random
+
+import nest_asyncio
+nest_asyncio.apply()
+
+client = discord.Client(intents=discord.Intents.default())
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
+starter_encouragements = [
+  "Cheer up!",
+  "Hang in there.",
+  "You are a great person!"
+]
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if msg.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+    
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
+keep_alive()
+client.run(os.getenv('TOKEN'))  # #'MTA3NzE0NTk1NTczODA3MTA2MA.G9Fn4X.KuLyCVg34kijK4rGqys9mWd8Pmi8TVA1lIGvLo'
