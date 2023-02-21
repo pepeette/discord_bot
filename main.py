c128f8ef5174a51329ebc80fd615576e0815e30f
pepeette <59664130+pepeette@users.noreply.github.com>
2023-02-21 15:46:00 +0700

    added commands about openai response

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ import os
 import requests
 import json
 import random
+import openai
+import discord
 
 import nest_asyncio
 nest_asyncio.apply()
 
-client = discord.Client(intents=discord.Intents.default())
+
+#load_dotenv()
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+OPENAI_KEY = os.getenv('OPENAI_KEY')
+
+# Set up the OpenAI API client
+openai.api_key = OPENAI_KEY
+
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents=intents)
 
 sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
 starter_encouragements = [
@@ -16,12 +27,13 @@ starter_encouragements = [
   "Hang in there.",
   "You are a great person!"
 ]
-
+prompt= "cheer me up"
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
   json_data = json.loads(response.text)
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return(quote)
+
 
 @client.event
 async def on_ready():
@@ -40,5 +52,21 @@ async def on_message(message):
     
   if any(word in msg for word in sad_words):
     await message.channel.send(random.choice(starter_encouragements))
+    
+  # Mention the client user
+  if prompt in message.content: #client.user in message.mentions:  
+    response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=prompt,  #f"{message.content}",
+    max_tokens=150,
+    temperature=0.8,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.3,
+    stop=["\n"]
+    )
+    # Send the response as a message
+    await message.channel.send(response.choices[0].text)
+    
 keep_alive()
-client.run(os.getenv('TOKEN'))  
+client.run(os.getenv('DISCORD_TOKEN'))  # #'MTA3NzE0NTk1NTczODA3MTA2MA.GD2rpA.5S_yKWGf5fDNxtM9LlVJxttpNeN71dPZkl5UMs'
